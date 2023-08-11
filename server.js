1575440644eea295ac413ce099e860c6f21fc43e
uppercod <matias.trujillo@blue.cl>
2023-08-11 11:34:15 -0400

    slots

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 import "atomico/ssr/load";
 import { h } from "atomico";
+import * as parse5 from "parse5";
 /**
  *
  * @param {string} name
  * @param {string} children
  */
-const slotWrapper = (name, children) =>
-  `<astro-slot${name ? ` slot="${name}"` : ""}>${children}</astro-slot>`;
+const slotWrapper = (name, children) => {
+  if (name === "default" || !name) return children;
+
+  const fragment = parse5.parseFragment(`${children}`);
+
+  // Add the missing slot attribute to child Element nodes
+  for (const node of fragment.childNodes) {
+    if (node.tagName && !node.attrs.some(({ name }) => name === "slot")) {
+      node.attrs.push({ name: "slot", value: name });
+    }
+  }
+
+  return parse5.serialize(fragment);
+};
 
 /**
  * @type {import("astro").SSRLoadedRenderer["ssr"]}
